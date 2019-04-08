3947d6b043b6974557cf612f6a9aeec8dc65675a
Aeres-Blade <aeresblade@gmail.com>
2019-04-08 15:04:00 +0200

    log questionnaire id

--- src/controller/QuestionnaireController.mjs
+++ src/controller/QuestionnaireController.mjs
@@ -64,7 +64,7 @@ export default class QuestionnaireController
 
 			if(questionnaire === null)
 			{
-				console.log('todo questionnaire not found');
+				console.log('todo questionnaire not found ' + request.data._id);
 				return;
 			}
 
